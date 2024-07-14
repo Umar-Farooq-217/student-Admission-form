2c91a49a22a7ca3c07f9ee00ce381528eb24a1ff
Umar Farooq <uf29664@gmail.com>
2024-07-14 09:05:41 -0700

    form

--- app/page.js
+++ app/page.js
@@ -1,10 +1,70 @@
+'use client'
 import './globals.css'
 
 export default function Home() {
+  
   return (
    <div>
     <div className='back bg-cover w-full '>
     <h1 className='text-5xl text-gradient  font-sans font-bold text-center'>Student Admission Form</h1>
+
+    <div className="mx-64">
+    <form >
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="name">
+            Name
+          </label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+           
+          
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            required
+          />
+        </div>
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
+            Email
+          </label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+           
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            required
+          />
+        </div>
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="phone">
+            Phone
+          </label>
+          <input
+            type="tel"
+            id="phone"
+            name="phone"
+         
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            required
+          />
+        </div>
+       
+       
+        <div className="flex items-center justify-between">
+          <button
+            type="submit"
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+          >
+            Submit
+          </button>
+        </div>
+      </form>
+        
+    
+      
+    </div>
 
     </div>
    </div>
